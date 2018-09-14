5f337c5208ebb6fcc0fc7139b5606d0618f552f0
fiyc <626897681@qq.com>
2018-09-14 11:09:36 +0800

    publish

--- resource/render.js
+++ resource/render.js
@@ -11,24 +11,69 @@ function init() {
     let searchInput = document.getElementById('search-input');
     let data = [
         {
-            title: "Python 装饰器理解 (1)",
-            url: "article/python_decorator_1.html",
-            date: "2018-09-11 14:07"
+            title: "emacs 改造之一场由代码折叠引发的血案",
+            url: "article/hack-emacs-one.html",
+            date: "2018-03-11 03:33:58"
         },
         {
-            title: "Python 装饰器理解 (2)",
-            url: "article/python_decorator_2.html",
-            date: "2018-09-11 14:07"
+            title: "Github + Hexo 搭建博客",
+            url: "article/hexo.html",
+            date: "2017-10-28 19:06:35"
         },
         {
-            title: "测试语法",
-            url: "article/test.html",
-            date: "2018-01-01 10:12"
+            title: "JavaScript设计模式 - 构造器模式",
+            url: "article/javascript-constructor-pattern.html",
+            date: "2018-02-17 11:27:57"
+        },
+        {
+            title: "JavaScript设计模式 - 模块化模式",
+            url: "article/javascript-module-pattern.html",
+            date: "2018-03-06 17:44:00"
+        },
+        {
+            title: "npm设置国内源",
+            url: "article/npm-domestic-source.html",
+            date: "2017-11-17 10:11:33"
+        },
+        {
+            title: "Spring 装配bean基础",
+            url: "article/spring-wiring-bean.html",
+            date: "2018-04-23 23:06:29"
+        },
+        {
+            title: "Spring学习笔记",
+            url: "article/spring-note.html",
+            date: "2018-04-25 22:43:23"
+        },
+        {
+            title: "实现简单的神经网络",
+            url: "article/machine-learning-demo.html",
+            date: "2018-02-14 23:05:07"
+        },
+        {
+            title: "拖拽上传文件实现过程记录",
+            url: "article/node-dragfile.html",
+            date: "2018-03-30 13:30:55"
         },
         {
             title: "机器学习实战 k-近邻算法",
-            url: "article/machine_learn_knn.html",
+            url: "article/machine-learning-knn.html",
             date: "2018-02-25 19:15:50"
+        },
+        {
+            title: "机器学习实战 使用香农熵划分数据集",
+            url: "article/machine-learning-shannon.html",
+            date: "2018-03-04 01:24:28"
+        },
+        {
+            title: "深入浅出正则表达式【转载】",
+            url: "article/regular-express.html",
+            date: "2018-02-25 17:14:14"
+        },
+        {
+            title: "跨域问题解析",
+            url: "article/ajax-cross-domain.html",
+            date: "2018-02-20 16:08:49"
         },
     ];
 
